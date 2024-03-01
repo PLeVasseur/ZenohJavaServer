e18e0165716466e4b31034e4c3a83f212bdeba72
Peter LeVasseur <peter.levasseur@gm.com>
2024-03-01 09:26:17 -0500

    Put in an experiment using unix domain sockets.

--- app/src/main/java/android/debug/zenohjavaserver/ZenohSubber.kt
+++ app/src/main/java/android/debug/zenohjavaserver/ZenohSubber.kt
@@ -3,6 +3,7 @@ package android.debug.zenohjavaserver
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import io.zenoh.Config
 import io.zenoh.Session
 import io.zenoh.keyexpr.KeyExpr
 import io.zenoh.query.Reply
@@ -18,8 +19,16 @@ class ZenohSubber {
         fun periodicallySubFromZenoh() {
             Log.d("ZenohDemo", "entered periodicallyGetFromZenoh()");
             try {
-                Session.open().use { session ->
-                    KeyExpr.tryFrom("demo/example/**").use { keyExpr ->
+                val jsonConfig = """
+                                 {
+                                   "mode": "peer",
+                                   "connect": { "endpoints": ["unixsock-stream//tmp/zenoh_java_socket"] }
+                                 }
+                                 """.trimIndent()
+                val config: Config = Config.from(jsonConfig)
+                Session.open(config).use { session ->
+                // Session.open().use { session ->
+                    KeyExpr.tryFrom("demo/example/zenoh-java-pub").use { keyExpr ->
                         Log.d("SessionStatus", "Declaring Subscriber on '$keyExpr'...")
                         session.declareSubscriber(keyExpr).res().use { subscriber ->
                             val receiver: BlockingQueue<Optional<Sample>>? = subscriber.receiver
